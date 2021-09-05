8aeb796be444c9dc6786d6540649db4ee40670f7
Hannah Malm <hannahemalm616@gmail.com>
2021-09-04 20:51:18 -0400

    commit tree 0ce746cebe7780731d03bf7f246e2e4e4792514c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,33 @@
 class BirdsController < ApplicationController
+  #render all of the birds in the index
   def index
     @birds = Bird.all
-    render json: @birds
+    render json: birds
   end
+
+  #render a specific bird after finding it by id
+  def show
+    bird = Bird.find_by(id: params[:id])
+    #render json: bird  <- This sends everything 
+    #render json: {id: bird.id, name: bird.name, species: bird.species } 
+    #render json: bird.slice(:id, :name, :species)
+    #render json: birds, only: [:id, :name, :species]
+    #render json: birds, except: [:created_at, :updated_at]
+    #render json: birds.to_json(except: [:created_at, :updated_at])
+    #THERE ARE MANY DIFFERENT WAYS TO WRITE THIS
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+  end
+
+  #show will produce the following minus the created at and updated at
+   # {
+  #   "id": 2,
+  #   "name": "Grackle",
+  #   "species": "Quiscalus Quiscula",
+  #   "created_at": "2019-05-09T21:51:41.543Z",
+  #   "updated_at": "2019-05-09T21:51:41.543Z"
+  # }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show' # new
+  #We can use multiple routes to differentiate between specific requests. 
+  #In an API, these are typically referred to as endpoints.
 end
